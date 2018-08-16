e6496fc09d656e3293fa8bb622633c16e3460ceb
RÆVENS-PC\Chloe <raeven@live.dk>
2018-08-16 21:43:38 +0200

    Movement and camera control

---
+++ TrashyPandas/Assets/ClickMovement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickMovement : MonoBehaviour {
+
+    CharacterController2D controller;
+    RectTransform rect;
+    [SerializeField] private Vector2 clickPosition;
+    [SerializeField] float width;
+    [SerializeField] float offset;
+    [SerializeField] private bool isHeld;
+
+    private void Start()
+    {
+        controller = FindObjectOfType<CharacterController2D>();
+        rect = GetComponent<RectTransform>();
+    }
+
+    private void FixedUpdate()
+    {
+        controller.IsMovingWithClick = isHeld;
+
+        if (!isHeld) return;
+
+        clickPosition = Input.mousePosition;
+        float direction = 0;
+
+        if (clickPosition.x > (width / 2) + offset)
+            direction = 1;
+
+        else if (clickPosition.x < (width / 2) - offset)
+            direction = -1;
+
+        controller.Move(direction);
+    }
+
+    public void OnClick()
+    {
+        if (CombatManager.InCombat) return;
+
+        isHeld = true;
+        width = rect.rect.width;
+    }
+
+    public void OnRelease()
+    {
+        
+        isHeld = false;
+    }
+
+}
---
+++ TrashyPandas/Assets/Scripts/CharacterController2D.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController2D : MonoBehaviour {
+
+    [SerializeField] private float speed;
+    [Range(0, .3f)] [SerializeField] private float m_MovementSmoothing = .05f;
+
+    private bool isMovingWithClick;
+
+    private Rigidbody2D rigid;
+    private bool canMove = true;
+    private Vector3 m_Velocity = Vector3.zero;
+
+    private Animator[] animators;
+
+    private void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        ICharacter[] units = GetComponentsInChildren<ICharacter>();
+        animators = new Animator[units.Length];
+        for (int i = 0; i < units.Length; i++)
+        {
+            animators[i] = units[i].GetComponent<Animator>();
+        }
+
+        CombatManager.instance.combatStartedDelegate += Stop;
+    }
+
+
+    private void FixedUpdate()
+    {
+        canMove = !CombatManager.InCombat;
+
+
+        if (canMove && !isMovingWithClick)
+        {
+            Move(Input.GetAxis("Horizontal"));
+        }
+    }
+
+    public void Move(float direction)
+    {
+        if (CombatManager.InCombat || !canMove)
+        {
+         
+
+        }
+        float deltaX = (direction * speed * Time.deltaTime);
+        Vector3 targetVelocity = new Vector2(deltaX, 0);
+
+
+        rigid.velocity = Vector3.SmoothDamp(rigid.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
+        SetAnim(deltaX);
+
+    }
+
+
+    private void SetAnim(float speed)
+    {
+        if (animators.Length > 0)
+        {
+            foreach (Animator animator in animators)
+            {
+                animator.SetFloat("Speed", speed);
+            }
+        }
+    }
+
+    public bool IsMovingWithClick
+    {
+        get
+        {
+            return isMovingWithClick;
+        }
+
+        set
+        {
+            isMovingWithClick = value;
+        }
+    }
+
+    public void Stop()
+    {
+        Debug.Log("Stop moving!");
+        SetAnim(0);
+    }
+
+}
